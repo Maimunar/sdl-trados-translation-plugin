2353f34b06283a883f073973abd8a89cce7d64ee
Aleksandar <i425981@fhict.nl>
2020-10-02 15:38:38 +0200

    User Stories Create a button and Hello World popup done

---
+++ HelloWorldPlugin/HelloWorldPlugin/HelloWorldAction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.DefaultLocations;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Extensions;
+
+namespace HelloWorldPlugin
+{
+    //Creates an action with id HelloWorldAction (like the class), a display name "Hello World!", a description and an Icon, which it gets from PluginResources.resx
+    //Puts it in the created Ribbon group with z-index 10 and Display Type - Large
+    [Action("HelloWorldAction", Name = "Hello World!", Description = "Display Hello World", Icon = "rocket")]
+    [ActionLayout(typeof(HelloWorldRibbonGroup), 10, DisplayType.Large)]
+    class HelloWorldAction : AbstractAction
+    {
+        //Executes on button click
+        protected override void Execute()
+        {
+            System.Windows.Forms.MessageBox.Show("Hello World!");
+        }
+    }
+
+}
---
+++ HelloWorldPlugin/HelloWorldPlugin/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorldPlugin
+{
+
+    class HelloWorldController 
+    {
+       
+    }
+}
