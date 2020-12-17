0854366662e3b9d2becf4b91d4dd659349c12811
martin-r-georgiev <martin.r.georgiev@hotmail.com>
2020-12-17 19:38:55 +0200

    Slight changes to paragraph processing

--- GCSTranslationMemory/GCSTranslationMemory/FileReader.cs
+++ GCSTranslationMemory/GCSTranslationMemory/FileReader.cs
@@ -91,7 +91,12 @@ namespace GCSTranslationMemory
                             XmlNodeList nodeList = requestHandler.Execute(pair.Item1, pair.Item2);
 
                             foreach (XmlNode node in nodeList)
+                            {
                                 CreateEURLEXTranslationMemory(node.InnerText, ref tm);
+                                break;
+                            }
+                                
+
                         }
                         //CreateEURLEXTranslationMemory("32006R2031", ref tm);
                         referenceNumbers.Add(match.Value);
