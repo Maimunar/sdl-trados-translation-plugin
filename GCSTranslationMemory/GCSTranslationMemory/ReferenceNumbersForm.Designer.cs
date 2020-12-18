c2f1d251f84070fc543cb6a735abdbc867ba5dff
martin-r-georgiev <martin.r.georgiev@hotmail.com>
2020-12-18 22:23:19 +0200

    Fixed translation memory error log text wrapping

--- GCSTranslationMemory/GCSTranslationMemory/ReferenceNumbersForm.Designer.cs
+++ GCSTranslationMemory/GCSTranslationMemory/ReferenceNumbersForm.Designer.cs
@@ -29,6 +29,7 @@ namespace GCSTranslationMemory
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.lblRefNumbers = new System.Windows.Forms.Label();
             this.rtbRefNumbers = new System.Windows.Forms.RichTextBox();
             this.lblErrorList = new System.Windows.Forms.Label();
@@ -70,6 +71,14 @@ namespace GCSTranslationMemory
             this.errorDataGrid.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.errorDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.errorDataGrid.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.errorDataGrid.DefaultCellStyle = dataGridViewCellStyle1;
             this.errorDataGrid.Location = new System.Drawing.Point(12, 231);
             this.errorDataGrid.Name = "errorDataGrid";
             this.errorDataGrid.RowHeadersVisible = false;
